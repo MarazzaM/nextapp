dd2a0fd09337d7e38a04b9363a12c4725463fcbc
LastLightbringer <89055276+MarazzaM@users.noreply.github.com>
2021-10-14 14:26:02 -0300

    s

--- next.config.js
+++ next.config.js
@@ -2,10 +2,3 @@
 module.exports = {
   reactStrictMode: true,
 }
-module.exports = {
-  images: {
-    domains: ["localhost"],
-    // next line is not required
-    path: 'http://localhost:3000/public'
-  }
-};
